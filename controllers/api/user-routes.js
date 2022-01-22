c8f7f1be49b7d44823d56055c517f97d5d2551ef
Mark Dale <msdaledad@gmail.com>
2022-01-21 21:43:35 -0500

    login, logout, signup all work...next post content and comments

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Comment, Vote } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // get all users
@@ -23,7 +23,7 @@ router.get('/:id', (req, res) => {
     include: [
       {
         model: Post,
-        attributes: ['id', 'title', 'post_url', 'created_at']
+        attributes: ['id', 'title', 'content', 'created_at']
       },
       {
         model: Comment,
@@ -32,12 +32,6 @@ router.get('/:id', (req, res) => {
           model: Post,
           attributes: ['title']
         }
-      },
-      {
-        model: Post,
-        attributes: ['title'],
-        through: Vote,
-        as: 'voted_posts'
       }
     ]
   })
@@ -54,11 +48,10 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', withAuth, (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+router.post('/', (req, res) => {
+  // expects {username: 'Lernantino', password: 'password1234'}
   User.create({
     username: req.body.username,
-    email: req.body.email,
     password: req.body.password
   })
     .then(dbUserData => {
@@ -77,14 +70,14 @@ router.post('/', withAuth, (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  // expects {username: 'lernantino', password: 'password1234'}
   User.findOne({
     where: {
-      email: req.body.email
+      username: req.body.username
     }
   }).then(dbUserData => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res.status(400).json({ message: 'No user with that username!' });
       return;
     }
 
@@ -116,7 +109,7 @@ router.post('/logout', (req, res) => {
 });
 
 router.put('/:id', withAuth, (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  // expects {username: 'Lernantino', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Blog, User, Comment } = require('../models');
+const { Post, User, Comment } = require('../models');
 
 // get all blogs for homepage
 router.get('/', (req, res) => {
   console.log('======================');
-  Blog.findAll({
+  Post.findAll({
     attributes: [
       'id',
       'title',
@@ -49,10 +49,9 @@ router.get('/post/:id', (req, res) => {
     },
     attributes: [
       'id',
-      'post_url',
       'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      'content',
+      'created_at'
     ],
     include: [
       {
@@ -95,6 +94,10 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
 });
 
 module.exports = router;
